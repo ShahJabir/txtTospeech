1562ad4e7d5d047f87ba4dbed09db409be11d5d1
Shah Jabir <58883768+ShahJabir@users.noreply.github.com>
2020-05-26 23:26:15 +0600

    txtTospeech.py added
    txtTospeech.py added

---
+++ txtTospeech.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import os
+
+msg: str
+langu = 'en'
+name: str
+
+msg = input("Write a message:")
+name = input("Name of your audio file:")
+
+obj = gTTS(text=msg, lang=langu, slow=False)
+obj.save(name+'.mp4')
+os.system('start Virus.mp4')

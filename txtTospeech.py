12dd3019e121cc2c92f01af806beb7a4eeb263e4
Shah Jabir <58883768+ShahJabir@users.noreply.github.com>
2020-05-27 16:18:14 +0600

    txtTospeech updated
    In this update, you can use it in windows, mac, and Linux OS.

--- txtTospeech.py
+++ txtTospeech.py
@@ -4,10 +4,18 @@ import os
 msg: str
 langu = 'en'
 name: str
+OS: int
 
 msg = input("Write a message:")
 name = input("Name of your audio file:")
+print("which OS are you use ?\n1.Windows\n2.mac\n3.linux")
+OS = input()
 
 obj = gTTS(text=msg, lang=langu, slow=False)
 obj.save(name+'.mp4')
-os.system('start '+name+'.mp4')
+if OS == 1:
+    os.system("start " + name + ".mp4")
+elif OS == 2:
+    os.system("open " + name + ".mp4")
+elif OS == 3:
+    os.system("mpg123 " + name + ".mp4")
